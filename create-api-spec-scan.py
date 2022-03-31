9ee4939e0021e6ca982d35cebc3233affce03a5b
Chris Tyson <christyson@users.noreply.github.com>
2022-03-31 14:05:51 -0400

    Update create-api-spec-scan.py

--- create-api-spec-scan.py
+++ create-api-spec-scan.py
@@ -129,9 +129,9 @@ try:
            response = res.json()
            print("Error encountered: " + response['_embedded']['errors'][0]['detail'] + " Error: " + response['_embedded']['errors'][0]['meta']['invalid_spec_error']['error_type'])
            sys.exit(1)
-       except:
-           print("Error has occurred")
-           sys.exit(1)
+    except:
+        print("Error has occurred")
+        sys.exit(1)
 except:
     #not found create a new one
     print("Creating a new API Specification")
