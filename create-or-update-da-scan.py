17735471b4c9d8d7aa5d2d1f02067e81f626dd26
Chris Tyson <christyson@users.noreply.github.com>
2022-10-24 16:08:19 -0400

    Update create-or-update-da-scan.py

--- create-or-update-da-scan.py
+++ create-or-update-da-scan.py
@@ -61,10 +61,10 @@ def prepared_request(method, end_point, json=None, query=None, file=None):
 
 # code above this line is reusable for all/most API calls
 
-res = prepared_request('GET','https://api.veracode.com/appsec/v1/applications/?name=' + dynamic_job+os.getenv("PIPELINE_ID"))
+res = prepared_request('GET','https://api.veracode.com/appsec/v1/applications/?name=' + dynamic_job)
 response = res.json()
 try:
-    print("looked for app" + dynamic_job+os.getenv("PIPELINE_ID"))
+    print("looked for app" + dynamic_job)
     print("Status code: " + str(res.status_code) )
     response = res.json()
     print("Response is: " + str(response))
@@ -74,10 +74,10 @@ except:
     print("Error executing API Call")
     sys.exit(1)
 
-print("Looking for Dynamic Analysis Job: " + dynamic_job+os.getenv("PIPELINE_ID") )
+print("Looking for Dynamic Analysis Job: " + dynamic_job )
 
 #Retrieve DA Job ID by project name
-res = prepared_request('GET', 'https://api.veracode.com/was/configservice/v1/analyses', query=("name=" + dynamic_job+os.getenv("PIPELINE_ID")))
+res = prepared_request('GET', 'https://api.veracode.com/was/configservice/v1/analyses', query=("name=" + dynamic_job))
 response = res.json()
 try:
     job_id = response['_embedded']['analyses'][0]['analysis_id']
@@ -85,7 +85,7 @@ except:
     print("Could not find Dynamic Analysis - Create one")
     #Payload for creating and scheduling new DA job
     data =   {
-      "name": dynamic_job+os.getenv("PIPELINE_ID"),
+      "name": dynamic_job,
       "scans": [
         {
           "linked_platform_app_uuid": uuid,  
