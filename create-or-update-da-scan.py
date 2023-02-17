e0d0d09ad9c4cff803ffb2b27484d4a7218d8d3b
Chris Tyson <christyson@users.noreply.github.com>
2023-02-17 11:12:09 -0500

    Update create-or-update-da-scan.py

--- create-or-update-da-scan.py
+++ create-or-update-da-scan.py
@@ -61,18 +61,18 @@ def prepared_request(method, end_point, json=None, query=None, file=None):
 
 # code above this line is reusable for all/most API calls
 
-res = prepared_request('GET','https://api.veracode.com/appsec/v1/applications/?name=' + dynamic_job)
-response = res.json()
-try:
-    print("looked for app:" + dynamic_job)
-    print("Status code: " + str(res.status_code) )
-    response = res.json()
-#    print("Response is: " + str(response))
-    uuid = response['_embedded']['applications'][0]['guid']
-except:
-    print("response failed: "+ res.status_code)
-    print("Error executing API Call")
-    sys.exit(1)
+#res = prepared_request('GET','https://api.veracode.com/appsec/v1/applications/?name=' + dynamic_job)
+#response = res.json()
+#try:
+#    print("looked for app:" + dynamic_job)
+#    print("Status code: " + str(res.status_code) )
+#    response = res.json()
+##    print("Response is: " + str(response))
+#    uuid = response['_embedded']['applications'][0]['guid']
+#except:
+#    print("response failed: "+ res.status_code)
+#    print("Error executing API Call")
+#    sys.exit(1)
 
 print("Looking for Dynamic Analysis Job: " + dynamic_job )
 
